642185733e63a4df1c540fb58ec2244ccfc0e3ac
John Else <john.else@citrix.com>
2015-10-08 16:27:22 +0100

    Initial commit
    Signed-off-by: John Else <john.else@citrix.com>

---
+++ Dockerfile
@@ -0,0 +1,23 @@
+FROM                        ubuntu:14.04
+MAINTAINER                  John Else <john.else@gmail.com>
+
+RUN     apt-get update
+RUN     apt-get -y install \
+            ipython \
+            ocaml \
+            openssh-server \
+            vim
+
+EXPOSE  22
+
+RUN     ln -sf /bin/bash /bin/sh
+RUN     mkdir /var/run/sshd
+
+RUN     useradd dev
+RUN     echo "dev:dev" | chpasswd
+RUN     echo "dev ALL=(ALL:ALL) NOPASSWD: ALL" >> /etc/sudoers
+RUN     mkdir /home/dev
+RUN     chown dev /home/dev
+
+CMD     ["/usr/sbin/sshd", "-D"]
+
---
+++ run.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+
+"""
+Helper script for launching a basic developer environment in a docker container.
+"""
+
+import argparse
+import os
+import subprocess
+import sys
+
+
+DEFAULT_ADDRESS = '127.0.0.1'
+DEFAULT_PORT = 12345
+
+
+def main():
+    """
+    Main entry point.
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--address', default=DEFAULT_ADDRESS,
+                        help='Host address on which the container will listen')
+    parser.add_argument('--port', default=DEFAULT_PORT, type=int,
+                        help='Host port which will forwarded to the '
+                             'container\'s listening port')
+
+    args = parser.parse_args(sys.argv[1:])
+    image = "%s/devenv" % os.getenv("USER")
+    docker_args = [
+        "docker", "run", "-d", "--name", "devenv-container-%d" % args.port,
+        "-p", "%s:%d:22" % (args.address, args.port),
+        image
+        ]
+    print "Launching docker with args %s" % docker_args
+    subprocess.call(docker_args)
+
+
+if __name__ == "__main__":
+    main()
