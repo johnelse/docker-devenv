5fbfeac814a082bb91ff97679b6333c6ef3d0ac4
John Else <john.else@citrix.com>
2015-10-14 16:29:21 +0100

    Use append instead of +=
    Signed-off-by: John Else <john.else@citrix.com>

--- run.py
+++ run.py
@@ -42,7 +42,7 @@ def main():
             ext_path = os.path.abspath(localdir)
             int_path = os.path.basename(ext_path)
             docker_args += ["-v", "%s:/external/%s:ro" % (ext_path, int_path)]
-    docker_args += [CONTAINER]
+    docker_args.append(CONTAINER)
     print "Launching docker with args %s" % docker_args
     subprocess.call(docker_args)
 
