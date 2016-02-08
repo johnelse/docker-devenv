d608cffe7abf46bce9a769a0efc1a0a60890ed39
John Else <john.else@citrix.com>
2016-02-08 14:12:27 +0000

    Test public key support
    Signed-off-by: John Else <john.else@citrix.com>

--- test/travis-test.sh
+++ test/travis-test.sh
@@ -2,11 +2,24 @@
 
 set -eux
 
+ssh-keygen -q -t rsa -f ~/.ssh/travis_rsa -N ""
+
 make
 
 mkdir -p /tmp/mount-dir
+echo "hello world" > /tmp/mount-dir/file
 
-CONTAINER=`./run.py --address 127.0.0.1 --port 12345 --dir /tmp/mount-dir`
+CONTAINER=`./run.py --address 127.0.0.1 --port 12345 \
+    --owner-pubkey ~/.ssh/travis_rsa.pub \
+    --guest-pubkey ~/.ssh/travis_rsa.pub \
+    --dir /tmp/mount-dir`
+
+sleep 5
+
+ssh -i ~/.ssh/travis_rsa -p 12345 -o StrictHostKeyChecking=no \
+    owner@127.0.0.1 "cat /external/mount-dir/file"
+ssh -i ~/.ssh/travis_rsa -p 12345 -o StrictHostKeyChecking=no \
+    guest@127.0.0.1 "cat /external/mount-dir/file"
 
 docker stop $CONTAINER
 docker rm $CONTAINER
