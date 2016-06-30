0768b6a1d194a62db7256ebfe816674d2334aae0
John Else <john.else@citrix.com>
2016-06-30 16:53:07 +0100

    test: use two different ssh keys
    Signed-off-by: John Else <john.else@citrix.com>

--- test/travis-test.sh
+++ test/travis-test.sh
@@ -2,7 +2,8 @@
 
 set -eux
 
-ssh-keygen -q -t rsa -f ~/.ssh/travis_rsa -N ""
+ssh-keygen -q -t rsa -f ~/.ssh/travis_rsa_owner -N ""
+ssh-keygen -q -t rsa -f ~/.ssh/travis_rsa_guest -N ""
 
 make
 
@@ -10,15 +11,15 @@ mkdir -p /tmp/mount-dir
 echo "hello world" > /tmp/mount-dir/file
 
 CONTAINER=`./run.py --address 127.0.0.1 --port 12345 \
-    --owner-pubkey ~/.ssh/travis_rsa.pub \
-    --guest-pubkey ~/.ssh/travis_rsa.pub \
+    --owner-pubkey ~/.ssh/travis_rsa_owner.pub \
+    --guest-pubkey ~/.ssh/travis_rsa_guest.pub \
     --dir /tmp/mount-dir`
 
 sleep 5
 
-ssh -i ~/.ssh/travis_rsa -p 12345 -o StrictHostKeyChecking=no \
+ssh -i ~/.ssh/travis_rsa_owner -p 12345 -o StrictHostKeyChecking=no \
     owner@127.0.0.1 "cat /external/mount-dir/file"
-ssh -i ~/.ssh/travis_rsa -p 12345 -o StrictHostKeyChecking=no \
+ssh -i ~/.ssh/travis_rsa_guest -p 12345 -o StrictHostKeyChecking=no \
     guest@127.0.0.1 "cat /external/mount-dir/file"
 
 docker stop $CONTAINER
